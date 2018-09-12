90e54b7cffcd7c5bcd1da61fa198544f192e9748
Olha Tlumak <otlum@macmini2599.ddns.softservecom.com>
2018-09-12 18:09:41 +0300

    Added new tests and changes in code

--- WorkWithFiles/test/test_work_files_spec.rb
+++ WorkWithFiles/test/test_work_files_spec.rb
@@ -1,58 +1,64 @@
 require 'rspec'
-require '../work_files'
+require '../files'
 
-describe 'Testing WorkFiles module' do
+describe 'Testing WFile class' do
 
-  before(:all) do
-    @file_name = 'test1.txt'
-    @new_file_name = 'test2.txt'
-  end
+  FILE_NAME = 'test1.txt'
+  NEW_FILE_NAME = 'test2.txt'
 
   before(:each) do
     $stdout = StringIO.new
-    @obj = WorkFiles::Work.new
+    @obj = WFile.new
+    @output = ''
   end
 
   after(:each) do
-    File.delete(@file_name) if File.exist?(@file_name)
-    File.delete(@new_file_name) if File.exist?(@new_file_name)
+    File.delete(FILE_NAME) if File.exist?(FILE_NAME)
+    File.delete(NEW_FILE_NAME) if File.exist?(NEW_FILE_NAME)
+    @output = ''
   end
 
   context '#Creation:' do
     it 'new file' do
-      @obj.create(@file_name,'w')
-      expect(File.exist?(@file_name)).to be true
+      @obj.create(FILE_NAME,'w')
+      expect(File.exist?(FILE_NAME)).to be true
     end
 
     it 'existing file' do
-      @obj.create(@file_name, 'w')
-      @obj.create(@file_name, 'w')
+      @obj.create(FILE_NAME, 'w')
+      @obj.create(FILE_NAME, 'w')
       $stdout.rewind
       expect($stdout.gets.strip).to eq 'File is exist!'
     end
 
     it 'new file with wrong right access' do
-      @obj.create(@file_name,'e')
+      @obj.create(FILE_NAME,'e')
       $stdout.rewind
-      expect($stdout.gets.strip).to include("Filed to create '#{@file_name}' file with the following error: ")
+      expect($stdout.gets.strip).to include("Filed to create '#{FILE_NAME}' file with the following error: ")
     end
 
-    #output error
-    #it "new file with 'r' right access" do
-    #  expect(@obj.create(@file_name,'r')).to raise_error ()
-    #end
+    it "new file with 'r' right access" do
+      @obj.create(FILE_NAME,'r')
+      $stdout.rewind
+      expect($stdout.gets.strip).to include ("Filed to create '#{FILE_NAME}' file with the following error:")
+    end
+
+    it 'new file without right access' do
+      @obj.create(FILE_NAME)
+      expect(File.writable?(FILE_NAME)).to be true
+    end
   end
 
   context '#Renaming:' do
     it 'exist file' do
-      @obj.create(@file_name,'w')
-      @obj.rename(@file_name, @new_file_name)
-      expect(File.exist?(@file_name)).not_to be true
-      expect(File.exist?(@new_file_name)).to be true
+      @obj.create(FILE_NAME,'w')
+      @obj.rename(FILE_NAME, NEW_FILE_NAME)
+      expect(File.exist?(FILE_NAME)).not_to be true
+      expect(File.exist?(NEW_FILE_NAME)).to be true
     end
 
     it 'not exist file' do
-      @obj.rename(@file_name, @new_file_name)
+      @obj.rename(FILE_NAME, NEW_FILE_NAME)
       $stdout.rewind
       expect($stdout.gets.strip).to eq 'File not exist!'
     end
@@ -60,22 +66,99 @@ describe 'Testing WorkFiles module' do
 
   context '#Deleting:' do
     it 'exist file' do
-      @obj.create(@file_name,'w')
-      @obj.delete(@file_name)
-      expect(File.exist?(@file_name)).not_to be true
+      @obj.create(FILE_NAME,'w')
+      @obj.delete(FILE_NAME)
+      expect(File.exist?(FILE_NAME)).not_to be true
     end
 
     it 'not exist file' do
-      @obj.delete(@file_name)
+      @obj.delete(FILE_NAME)
       $stdout.rewind
       expect($stdout.gets.strip).to eq 'File not exist!'
     end
   end
 
-  #do!
-  #context 'Manipulation with file:' do
-  #
- # end
+  context 'Manipulation with file:' do
+    before(:each) do
+      @obj.create(FILE_NAME,'w')
+      File.open(FILE_NAME,'w') {|line| line.puts 'Hello'}
+    end
+
+    it "read file with 'r' right access" do
+      @obj.manipulation_with_file(FILE_NAME, 'r')
+      $stdout.rewind
+      expect($stdout.gets.strip).to eq 'Hello'
+    end
+
+    it "write file with 'w' right access and default text" do
+      @obj.manipulation_with_file(FILE_NAME,'w')
+      $stdout.rewind
+      expect($stdout.gets.strip).to eq ''
+    end
+
+    it "write file with 'w+' right access and default text" do
+      @obj.manipulation_with_file(FILE_NAME,'w+')
+      $stdout.rewind
+      expect($stdout.gets.strip).to eq ''
+    end
+
+    it "write file with 'a' right access and default text" do
+      @obj.manipulation_with_file(FILE_NAME,'a')
+      $stdout.rewind
+      expect($stdout.gets).to eq "Hello\n"
+    end
+
+    it "write file with 'a+' right access and default text" do
+      @obj.manipulation_with_file(FILE_NAME,'a+')
+      $stdout.rewind
+      expect($stdout.gets).to eq "Hello\n"
+    end
+
+    it "write file with 'w' right access and text ' word'" do
+      @obj.manipulation_with_file(FILE_NAME,'w', ' word')
+      $stdout.rewind
+      expect($stdout.gets.chomp).to eq ' word'
+    end
+
+    it "write file with 'w+' right access and text ' word'" do
+      @obj.manipulation_with_file(FILE_NAME,'w+', ' word')
+      $stdout.rewind
+      expect($stdout.gets.chomp).to eq ' word'
+    end
+
+    it "write file with 'a' right access and text ' word'" do
+      @obj.manipulation_with_file(FILE_NAME,'a', ' word')
+      $stdout.rewind
+      $stdout.each_line {|l| @output << l}
+      expect(@output.strip).to eq "Hello\n word"
+    end
+
+    it "write file with 'a+' right access and text ' word'" do
+      @obj.manipulation_with_file(FILE_NAME,'a+', ' word')
+      $stdout.rewind
+      $stdout.each_line {|l| @output << l}
+      expect(@output.strip).to eq "Hello\n word"
+    end
+
+    it "read file with 'r' right access and text ' word'" do
+      @obj.manipulation_with_file(FILE_NAME, 'r', ' word')
+      $stdout.rewind
+      expect($stdout.gets.strip).to eq 'Hello'
+    end
+
+    it "read file with wrong right access" do
+      @obj.manipulation_with_file(FILE_NAME, 'e')
+      $stdout.rewind
+      expect($stdout.gets.strip).to eq 'Wrong right access'
+    end
+
+    it "read not exist file" do
+      @obj.manipulation_with_file(NEW_FILE_NAME, 'r')
+      $stdout.rewind
+      expect($stdout.gets.strip).to eq 'File not exist!'
+    end
+
+  end
 
   context '#Output information about created files if:' do
     it 'files are not have been created' do
@@ -85,21 +168,21 @@ describe 'Testing WorkFiles module' do
     end
 
     it 'exist 1 file' do
-      @obj.create(@file_name,'w')
+      @obj.create(FILE_NAME,'w')
       @obj.out_info_about_file
       $stdout.rewind
-      expect($stdout.gets.strip).to eq "#{@file_name}: w"
+      expect($stdout.gets.strip).to eq "#{FILE_NAME}: w"
     end
-=begin
-do!
+
     it 'exist 2 files' do
-      @obj.create(@file_name,'w')
-      @obj.create(@new_file_name,'w')
+      @obj.create(FILE_NAME,'w')
+      @obj.create(NEW_FILE_NAME,'w')
       @obj.out_info_about_file
       $stdout.rewind
-      expect($stdout.gets.strip).to eq "#{@file_name}: w#{@new_file_name}: w"
+      $stdout.each_line {|l| @output << l}
+      expect(@output.strip).to eq "#{FILE_NAME}: w\n#{NEW_FILE_NAME}: w"
     end
-=end
+
   end
 
 end
